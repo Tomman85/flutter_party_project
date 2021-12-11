f85c7f0f33fc39b9b345b49fbbd5e5ffc78bf2c3
Tomasz Nowak <tomnow300@gmail.com>
2021-12-11 17:56:45 +0100

    Complete UI of party event application,

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -10,20 +10,81 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    var isPortrait = MediaQuery.of(context).orientation == Orientation.portrait;
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       body: Container(
-        decoration: BoxDecoration(
+        padding: const EdgeInsets.all(30),
+        width: double.infinity,
+        decoration: const BoxDecoration(
           image: DecorationImage(
             image: NetworkImage(
                 'https://images.unsplash.com/photo-1533219057257-4bb9ed5d2cc6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'),
             fit: BoxFit.cover,
           ),
         ),
-        child: Container(
+        child: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.end,
             crossAxisAlignment: CrossAxisAlignment.start,
-            children: [Text("elo")],
+            children: [
+              const SizedBox(
+                height: 160,
+              ),
+              const Text(
+                "Find the best locations near\nyou for a good night.",
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Text(
+                "Let us find you an event\nfor your interest",
+                style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.white.withOpacity(.7),
+                ),
+              ),
+              const SizedBox(
+                height: 150,
+              ),
+              InkWell(
+                child: Container(
+                  margin: isPortrait
+                      ? EdgeInsets.all(0)
+                      : EdgeInsets.symmetric(horizontal: 100),
+                  padding: isPortrait
+                      ? const EdgeInsets.symmetric(horizontal: 35)
+                      : const EdgeInsets.symmetric(horizontal: 60),
+                  height: isPortrait ? size.height * 0.08 : size.height * 0.2,
+                  decoration: const ShapeDecoration(
+                      shape: StadiumBorder(), color: Colors.orange),
+                  child: Row(
+                    children: const [
+                      Text(
+                        "Find nearest event",
+                        style: TextStyle(color: Colors.white, fontSize: 17),
+                      ),
+                      Spacer(),
+                      Icon(
+                        Icons.arrow_forward,
+                        color: Colors.white,
+                      )
+                    ],
+                  ),
+                ),
+                onTap: () {
+                  print('elo');
+                },
+              ),
+              const SizedBox(
+                height: 50,
+              )
+            ],
           ),
         ),
       ),
